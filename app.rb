21387f77391010a6b097ac4afdb47c63386cb065
Keesha Hargrove <kharg14@gmail.com>
2020-09-25 20:20:11 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,10 +2,9 @@ class App < Sinatra::Base
 
   # Define your GET '/' route below and respond with "Hello, World!"
 
+
+
+
 get '/' do
- responds with "Hello, World!"
-  end
-
-
-
+    "Hello, World!"
 end
